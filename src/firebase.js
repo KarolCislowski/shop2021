9e44ce0ac9c595db18f5118d950cee3212e10d1e
Karol Cislowski <cislowski.karol@hotmail.com>
2020-12-31 16:33:57 +0100

    Clean up

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBTcPZCTxfXFfvpN54iKJdm5_l3b90OwFM",
+  authDomain: "cislowski-shop2021.firebaseapp.com",
+  projectId: "cislowski-shop2021",
+  storageBucket: "cislowski-shop2021.appspot.com",
+  messagingSenderId: "650962294760",
+  appId: "1:650962294760:web:f9f42d8c4dfdd2db84f65d"
+}
