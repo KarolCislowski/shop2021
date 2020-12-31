a2fc74c553c79998c22fa7b35f9cd92e586e30d7
Karol Cislowski <cislowski.karol@hotmail.com>
2020-12-31 17:49:04 +0100

    Happy New Year!!

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import './App.css'
 
+import { Header } from './Components/Header/Header'
+
 function App() {
   return (
     <div className="app">
+      <Header />
       <h1>SHOP 2021</h1>
     </div>
   )
