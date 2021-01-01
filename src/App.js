ed0dbf1a06415eb00f0b98a60707e5e0a5fc76f2
Karol Cislowski <cislowski.karol@hotmail.com>
2021-01-01 19:23:31 +0100

    Router config

--- src/App.js
+++ src/App.js
@@ -1,14 +1,33 @@
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from 'react-router-dom'
+
 import './App.css'
 
+//Components
 import { Header } from './Components/Header/Header'
+
+//Pages
 import { Home } from './Pages/Home/Home'
 
 function App() {
   return (
-    <div className="app">
-      <Header />
-      <Home />
-    </div>
+    <Router>
+      <div className="app">
+        <Header />
+
+        <Switch >
+          <Route path='/checkout' >
+            <h1>Checkout goes here</h1>
+          </Route>
+          <Route path='/'>
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   )
 }
 
