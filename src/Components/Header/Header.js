6f3dfb797e36f197adc74f003b994ac048e00df8
Karol Cislowski <cislowski.karol@hotmail.com>
2021-01-01 14:31:40 +0100

    Header Ui

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,9 +1,32 @@
 import './Header.css'
+import SearchIcon from '@material-ui/icons/Search'
 
 export const Header = () => {
   return (
     <header className='header'>
-      HEADER
+      <h5 className='header__logo'>Red Dwarf</h5>
+
+      <div className='header__search'>
+        <input
+          className='header__searchInput'
+          type='text'
+        />
+        <SearchIcon className='header__searchIcon' />
+      </div>
+      <nav className='header__nav'>
+        <div className='header__option'>
+          <span className='header__option__lineOne'>Hello Guest</span>
+          <span className='header__option__lineTwo'>Sign in</span>
+        </div>
+        <div className='header__option'>
+          <span className='header__option__lineOne'>Returns</span>
+          <span className='header__option__lineTwo'>{`&`} Orders</span>
+        </div>
+        <div className='header__option'>
+          <span className='header__option__lineOne'>Your</span>
+          <span className='header__option__lineTwo'>Prime</span>
+        </div>
+      </nav>
     </header>
   )
 }
