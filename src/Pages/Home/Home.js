ff8cf4b792582982db3b767b2aca5abf44e7bdcc
Karol Cislowski <cislowski.karol@hotmail.com>
2021-01-07 00:58:18 +0100

    Rebranding

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -6,7 +6,7 @@ export const Home = () => {
     <>
       <img
         className='home__image'
-        src='https://images.unsplash.com/photo-1462332420958-a05d1e002413?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1340&q=80'
+        src='https://images.unsplash.com/photo-1544084944-15269ec7b5a0?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1951&q=80'
         alt='hero__image'
       />
       <div className='home'>
