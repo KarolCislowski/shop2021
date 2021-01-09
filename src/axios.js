f4a422a65b4c47e355c7136e45826b7ec74c4b00
Karol Cislowski <cislowski.karol@hotmail.com>
2021-01-09 19:12:03 +0100

    deployment

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const instance = axios.create({
-  baseURL: 'http://localhost:5001/cislowski-shop2021/us-central1/api'
+  baseURL: 'https://us-central1-cislowski-shop2021.cloudfunctions.net/api'
 })
 
 export default instance
