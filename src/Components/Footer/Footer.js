98fd4a9f6c7be21f0f750c99e6c2ff0b85dc9a49
Karol Cislowski <cislowski.karol@hotmail.com>
2021-01-09 18:45:06 +0100

    footer

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -2,6 +2,24 @@ import './Footer.css'
 
 export const Footer = () => (
   <footer className='footer'>
-    FOOTER
+    <div className='footer__container'>
+      <div className='footer__left'>
+        <h3 className='footer__sectionHeading'>ABOUT PROJECT:</h3>
+        <p>Trip Store is a project created to show my skills to potential Employees or Clients, hope you like it. </p>
+      </div>
+
+      <div className='footer__center'>
+        <h3 className='footer__sectionHeading'>ABOUT ME:</h3>
+        <p>I'm Karol Cislowski and I'm fullstack developer with focus on front end using React js library. </p>
+      </div>
+
+      <div className='footer__right'>
+        <h3 className='footer__sectionHeading'>Links:</h3>
+        <p>
+          <li><a href='https://cislowski-karol.com/'>Portfolio</a></li>
+          <li><a href='https://github.com/KarolCislowski'>GitHub</a></li>
+        </p>
+      </div>
+    </div>
   </footer>
 )
